9c2f086e59f1f45f16854da390bd44dceeee495b
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-30 18:40:18 -0300

    readme-updated

--- EnviromentConfig/config.js
+++ EnviromentConfig/config.js
@@ -4,11 +4,11 @@ const [create] = process.argv.slice(2)
 const arg = process.argv.slice(4)
 
 if(create === 'make:api'){
-    shell.exec('node ./EnviromentConfig/createApi.js')
+    shell.exec('node ./node_modules/api-template/EnviromentConfig/createApi.js')
 } else if (create === 'make:entity') {
     if(arg === 'c'){
-        shell.exec(`node ./EnviromentConfig/createFullEntity.js ${upperEntityName}`)
+        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createFullEntity.js ${upperEntityName}`)
     } else {
-        shell.exec(`node ./EnviromentConfig/createEntity.js ${upperEntityName}`)
+        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createEntity.js ${upperEntityName}`)
     }
 }
