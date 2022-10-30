7b363572571acff4f929fd31170dc199afdfb13c
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-30 19:17:38 -0300

    bug-fixes

--- EnviromentConfig/config.js
+++ EnviromentConfig/config.js
@@ -3,11 +3,15 @@ import shell from 'shelljs'
 const [create] = process.argv.slice(2)
 const arg = process.argv.slice(4)
 const [upperEntityName] = process.argv.slice(3)
-const entityName = `${upperEntityName.charAt(0).toUpperCase()}${upperEntityName.slice(1)}`
+let entityName = null
+
+if (upperEntityName !== undefined){
+    entityName = `${upperEntityName.charAt(0).toUpperCase()}${upperEntityName.slice(1)}`
+}
 
 if(create === 'make:api'){
     shell.exec('node ./node_modules/api-template/EnviromentConfig/createApi.js')
-} else if (create === 'make:entity') {
+} else if (create === 'make:entity' && entityName !== null) {
     if(arg === 'c'){
         shell.exec(`node ./node_modules/api-template/EnviromentConfig/createFullEntity.js ${entityName}`)
     } else {
