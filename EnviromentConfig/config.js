2159cc3cc68e7c611d5fb419122b1a058594ebb0
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-30 18:56:17 -0300

    test

--- EnviromentConfig/config.js
+++ EnviromentConfig/config.js
@@ -2,13 +2,15 @@ import shell from 'shelljs'
 
 const [create] = process.argv.slice(2)
 const arg = process.argv.slice(4)
+const [upperEntityName] = process.argv.slice(3)
+const entityName = `${upperEntityName.charAt(0).toUpperCase()}${upperEntityName.slice(1)}`
 
 if(create === 'make:api'){
     shell.exec('node ./node_modules/api-template/EnviromentConfig/createApi.js')
 } else if (create === 'make:entity') {
     if(arg === 'c'){
-        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createFullEntity.js ${upperEntityName}`)
+        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createFullEntity.js ${entityName}`)
     } else {
-        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createEntity.js ${upperEntityName}`)
+        shell.exec(`node ./node_modules/api-template/EnviromentConfig/createEntity.js ${entityName}`)
     }
 }
