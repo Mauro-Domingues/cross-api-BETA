a9e4150b5883e937f7cc2e9e94eb10fa48340f32
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-30 21:38:46 -0300

    files-updated

--- EnviromentConfig/templates/app.js
+++ EnviromentConfig/templates/app.js
@@ -1,15 +1,17 @@
-export function createApp(){
+export function createApp() {
     const newApp = `
     import express from 'express'
+    import { AppDataSource } from "./database/connection.js"
     import cors from './middlewares/cors.js'
     import exampleRoutes from './routes/example.js' // Mapeie suas rotas 
 
-    const app = express()
-
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(cors)
-    app.use('/', exampleRoutes) // Use as rotas pelo app
+    AppDataSource.initialize().then(() => {
+        const app = express()
+        app.use(express.json())
+        app.use(express.urlencoded({ extended: true }))
+        app.use(cors)
+        app.use('/example', exampleRoutes) // Use as rotas pelo app
+    })
 
     export default app`
     return newApp
--- EnviromentConfig/templates/model.js
+++ EnviromentConfig/templates/model.js
@@ -1,23 +1,42 @@
-export function createModel(arg, upperArg){
+export function createModel(arg, upperArg) {
     const createNewModel = `
-    import { Entity, Column, PrimaryGeneratedColumn, DateTime } from "typeorm"
+    import { Entity, Column, PrimaryGeneratedColumn, DateTime, OneToOne, OneToMany, ManyToOne, ManyToMany, JoinColumn, JoinTable } from "typeorm"
 
-    @Entity('${arg}s')
+    /** Para consultar os tipos de colunas:
+    https://github.com/typeorm/typeorm/blob/master/src/decorator/options/ColumnOptions.ts */
+
+    @Entity('${arg}')
     export class ${upperArg} {
         @PrimaryGeneratedColumn()
         id!: number
 
-        @Column({ type: 'text' })
-        name!: string
+        //                   //
+        // insira as colunas //
+        //                   //
 
-        @Column({ type: 'text', nullable: true})
-        description!: string
-
-        @column({ type: 'timestamp', autoCreate: true })
+        @column({ type: 'timestampz', onCreate: true })
         createdAt!: DateTime
 
-        @column({  type: 'timestamp', autoCreate: true, autoUpdate: true })
+        @column({  type: 'timestampz', onCreate: true, onUpdate: true })
         updatedAt!: DateTime
+
+        // Exemplos de relações sendo esse o principal
+
+        /*@OneToOne(() => Entity, entityColumn => entityColumn.this)
+        @JoinColumn({name: 'entity_id'})
+        entityColumn!: Relation<entityColumn>
+
+        @OneToMany(() => Entity, entityColumn => entityColumn.this)
+        entityColumn!: Entity[]
+
+        @ManyToOne(() => Entity, entityColumn => entityColumn.this)
+        @JoinColumn({name: 'entity_id'})
+        entityColumn!: Entity
+    
+        @ManyToMany(() => Entity, entityColumn => entityColumn.this)
+        @JoinTable() // especifica que esse é o principal da relação, deixe vazio no inverso
+        entityColumn!: Entity[]*/
+
     } // Ainda não funciona 100% pois precisa ser feito com typescript para tipar as colunas`
     return createNewModel
 }  
