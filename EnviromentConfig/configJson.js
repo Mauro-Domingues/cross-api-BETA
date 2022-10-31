a69751d67ecdb3d434c4864ccbd2182393dd7b44
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-31 01:03:28 -0300

    silly-fixes

--- EnviromentConfig/configJson.js
+++ EnviromentConfig/configJson.js
@@ -3,3 +3,7 @@ import fs from 'fs'
 fs.copyFile('./node_modules/api-template/EnviromentConfig/templates/package.json', 'package.json', (err) => {
     if (err) throw err
 })
+
+console.log("")
+console.log(`Digite "npm run cross" para ver os comandos`)
+console.log("")
