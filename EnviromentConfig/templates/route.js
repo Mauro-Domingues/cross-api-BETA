9f1c83471c09f6a0c2ca1db31f504b8891b2d9e5
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-30 22:35:21 -0300

    silly-fixes

--- EnviromentConfig/templates/route.js
+++ EnviromentConfig/templates/route.js
@@ -6,12 +6,12 @@ import { ${upperArg}Controller } from '../controllers/${upperArg}Controller.js'
 const router = Router()
 const ${arg}Controller = new ${upperArg}Controller()
 
-router.get('/', ${upperArg}Controller.get)
-router.get('/:id', ${upperArg}Controller.getById)
-router.get('/name/:name', ${upperArg}Controller.getByName)
-router.post('/', ${upperArg}Controller.create)
-router.put('/:id', ${upperArg}Controller.update)
-router.delete('/:id', ${upperArg}Controller.delete)
+router.get('/', ${arg}Controller.get)
+router.get('/:id', ${arg}Controller.getById)
+router.get('/name/:name', ${arg}Controller.getByName)
+router.post('/', ${arg}Controller.create)
+router.put('/:id', ${arg}Controller.update)
+router.delete('/:id', ${arg}Controller.delete)
 
 export default router`
     return createNewService
